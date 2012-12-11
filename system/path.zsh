791595ee77c1e47af204edcf404cdcb36eab1d33
Joe Roberts <joe@zefer.co.uk>
2012-12-11 10:26:26 +0000

    add global npm modules to the path, so I can run 'em

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
+# add global npm modules
+export PATH=/usr/local/share/npm/bin:$PATH
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
