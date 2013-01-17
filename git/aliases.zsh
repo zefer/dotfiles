baedd2856b987a20627c22d42b44654fdc93d675
Joe Roberts <joe@zefer.co.uk>
2013-01-17 09:18:04 +0000

    add a second git log format alias, a full colourised graph log

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,6 @@
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog2='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit -m'
