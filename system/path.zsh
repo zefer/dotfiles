85973179f0b8d29d40650597d2adee41bff8da17
Joe Roberts <joe@zefer.co.uk>
2013-01-25 12:33:35 +0000

    add local pyhton scripts to path

--- system/path.zsh
+++ system/path.zsh
@@ -3,4 +3,7 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 # add global npm modules
 export PATH=/usr/local/share/npm/bin:$PATH
 
+# add local python scripts
+export PATH=/usr/local/share/python:$PATH
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
