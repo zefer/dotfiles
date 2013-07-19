51d943cb31f257b6ce24a98007540b04638e06f9
Joe Roberts <joe@zefer.co.uk>
2013-07-19 11:07:46 +0100

    add alias to show git churn; file edit frequency leaderboard

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,3 +10,4 @@ alias gb='git branch'
 alias ga='git add'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gchurn='git log --name-only | grep .rb | sort | uniq -c | sort -nr | head'
