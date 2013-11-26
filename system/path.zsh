183dfafa432ac1e9ba08cd02267391ae1a559eb2
Joe Roberts <joe@zefer.co.uk>
2013-11-26 19:57:00 +0000

    homebrew no longer puts python scripts here

--- system/path.zsh
+++ system/path.zsh
@@ -3,9 +3,6 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 # add global npm modules
 export PATH=/usr/local/share/npm/bin:$PATH
 
-# add local python scripts
-export PATH=/usr/local/share/python:$PATH
-
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
