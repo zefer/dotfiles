1b586d870b18627511cb48b4e9e236b6176538b4
Joe Roberts <joe@zefer.co.uk>
2014-03-02 12:49:53 +0000

    Add command to play BBC radio stations

--- music/controls.zsh
+++ music/controls.zsh
@@ -5,6 +5,7 @@ alias pause='music_control pause'
 alias playing=music_status
 alias music="open $MUSIC_HOST"
 
+# usage: 'music_control play', music_control pause'
 function music_control() {
   curl -sI "$MUSIC_HOST/command/?cmd=$1" | head -n 1
   music_status
@@ -12,4 +13,15 @@ function music_control() {
 
 function music_status() {
   curl -s $MUSIC_HOST/_player_engine.php | jq '. | {state, bitrate, fileext, currentsong, currentalbum, currentartist}'
+}
+
+# usage 'radio', 'radio 4'. Defaults to 6 music
+function radio() {
+  station=${1:-6}
+  if [ $station -eq 6 ]; then
+    playlist="BBC $station Music"
+  else
+    playlist="BBC Radio $station"
+  fi
+  curl -X POST -d "path=WEBRADIO/$playlist.m3u" "$MUSIC_HOST/db/?cmd=addreplaceplay"
 }
