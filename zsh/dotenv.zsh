f5636c791e1afaa82f3f083ab8b7d00218f0341b
Joe Roberts <joe@zefer.co.uk>
2014-03-11 12:46:51 +0000

    grey echo output

--- zsh/dotenv.zsh
+++ zsh/dotenv.zsh
@@ -5,7 +5,8 @@ function chpwd {
 # exports all env vars from .env in current dir, if present
 function load_dotenv_vars {
   if [ -f ".env" ]; then
-    echo "adding .env to ENV"
+    # coloured output
+    echo "$(tput setaf 240)adding .env to ENV$(tput sgr0)"
     export $(cat .env)
   fi
 }
