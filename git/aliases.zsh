10c52d4b8ff4a77e2742bb1b374e6dcd35500418
Joe Roberts <joe@zefer.co.uk>
2014-09-11 15:20:10 +0100

    use yellow cos red was hard to read

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,7 @@
 alias git=hub
 
 alias gl='git up'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glog2='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 alias gp='git push origin HEAD'
 alias gd='git diff'
