2261a0a64a7c5b5c95577d9ea3147fdabc811cb8
Joe Roberts <joe@zefer.co.uk>
2017-05-15 15:03:31 +0100

    Remove unused RVM & mysql path config

--- system/path.zsh
+++ system/path.zsh
@@ -2,13 +2,7 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export PATH=/usr/local/heroku/bin:$PATH
 
-export PATH=$HOME/.rvm/bin:$PATH
-
 # add global npm modules
 export PATH=/usr/local/share/npm/bin:$PATH
 
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
 cdpath=($PROJECTS $GOPATH/src/github.com/zefer $GOPATH/src/github.com/airbrake)
-
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
