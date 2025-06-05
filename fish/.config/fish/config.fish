bbec0d8dca3c023ac435953e9ab76a24c9c247ba
Joe Roberts <joe@zefer.co.uk>
2025-06-05 15:43:11 +0100

    fix whitespace

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,6 @@
 set -gx EDITOR nvim
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
+  # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
 end
