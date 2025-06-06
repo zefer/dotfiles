ed170e86e13fb8c1ea9d8d53fab0eb598cd5e5e0
Joe Roberts <joe@zefer.co.uk>
2025-06-06 07:54:51 +0100

    Use fzf for history search

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,5 +5,5 @@ set -U fish_greeting
 
 if status is-interactive
   fish_vi_key_bindings
-  bind -M insert \cr history-pager
+  bind -M insert \cr 'history | fzf | read -l result; and commandline $result'
 end
